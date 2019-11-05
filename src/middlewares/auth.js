ddf57e211c437bbbaddf4e44a71c251d3f417966
Vinicius Melin <viniciusti.melin@hotmail.com>
2019-11-05 19:30:53 -0300

    fix: ajustes na autenticacao

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,20 +2,22 @@ const jwt = require('jsonwebtoken');
 const {promisify} = require('util');
 
 const auth = async(req, res, next) => {
-    const authHeader = req.headers.autorization;
+    
+    const {authorization} = req.headers;
 
     if(req.path === '/registrar' || req.path === '/autenticar' || req.path === '/api') {
         console.log(req.path)
         return next();
     }
 
-    if(!authHeader) {
+    if(!authorization) {
         return res.status(401).send({ error: 'No token provided'});
     }
 
 
 
-    const [schema, token] = authHeader.split(" ");
+    const  token = authorization.replace('Bearer ', '')
+
     try {
         const decoded = await promisify(jwt.verify)(token, "secret");
         req.userId = decoded.id;
